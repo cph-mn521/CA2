c4ef20cc7b44083fa6bd04e0649ec3552e52a9a0
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-10-04 18:12:33 +0200

    Create Address.java

---
+++ src/main/java/Entities/Address.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Niels Bang
+ */
+@Entity
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String Street, AdditionalInfo;
+
+    @ManyToOne
+    private CityInfo city;
+
+    private List<InfoEntity> info;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String Street) {
+        this.Street = Street;
+    }
+
+    public String getAdditionalInfo() {
+        return AdditionalInfo;
+    }
+
+    public void setAdditionalInfo(String AdditionalInfo) {
+        this.AdditionalInfo = AdditionalInfo;
+    }
+
+    public CityInfo getCity() {
+        return city;
+    }
+
+    public void setCity(CityInfo city) {
+        this.city = city;
+    }
+
+    public List<InfoEntity> getInfo() {
+        return info;
+    }
+
+    public void setInfo(List<InfoEntity> info) {
+        this.info = info;
+    }
+
+    public void addInfoEntity(InfoEntity info) {
+        this.info.add(info);
+    }
+
+}
