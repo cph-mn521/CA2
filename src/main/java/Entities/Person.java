397e8aa728e05222c0d1c01b8880e6d9e401d08a
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-10-04 18:13:33 +0200

    #107 Create Person Entity

---
+++ src/main/java/Entities/Person.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Niels Bang
+ */
+@Entity
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName, lastName;
+
+    @OneToOne
+    private InfoEntity info;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    private List<Hobby> hobbies;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, InfoEntity info, List<Hobby> hobbies) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.info = info;
+        this.hobbies = hobbies;
+    }
+
+    public InfoEntity getInfo() {
+        return info;
+    }
+
+    public void setInfo(InfoEntity info) {
+        this.info = info;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Hobby> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<Hobby> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public void addHobby(Hobby hobby) {
+        this.hobbies.add(hobby);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.firstName);
+        hash = 67 * hash + Objects.hashCode(this.lastName);
+        hash = 67 * hash + Objects.hashCode(this.info);
+        hash = 67 * hash + Objects.hashCode(this.hobbies);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.info, other.info)) {
+            return false;
+        }
+        if (!Objects.equals(this.hobbies, other.hobbies)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", info=" + info + ", hobbies=" + hobbies + '}';
+    }
+
+}
