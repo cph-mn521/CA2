dbba96ccfb30c12f9ec4e5eea0b1aa84bc0e606d
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-10-07 13:16:41 +0200

    updated connectionstr

--- src/main/java/util/EMF_Creator.java
+++ src/main/java/util/EMF_Creator.java
@@ -104,7 +104,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_STR2");
+            connection_str = System.getenv("CONNECTION_STR3");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
